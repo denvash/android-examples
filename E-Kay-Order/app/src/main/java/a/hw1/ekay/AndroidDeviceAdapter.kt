96116a8475f2b17ed9ffa26b5b22e55024497060
denvash <justdenva@gmail.com>
2018-11-07 23:20:49 +0200

    Single Selection done

--- E-Kay-Order/app/src/main/java/a/hw1/ekay/AndroidDeviceAdapter.kt
+++ E-Kay-Order/app/src/main/java/a/hw1/ekay/AndroidDeviceAdapter.kt
@@ -6,10 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckedTextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_order_activity.*
 
 
 class AndroidDeviceAdapter(internal var context: Context, private val devices: Array<String>) :
     RecyclerView.Adapter<AndroidDeviceAdapter.MyViewHolder>() {
+
+    private lateinit var lastChanged : CheckedTextView
+    private var initilized = false
 
     inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         internal var simpleCheckedTextView: CheckedTextView =
@@ -23,11 +27,25 @@ class AndroidDeviceAdapter(internal var context: Context, private val devices: A
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
-
         holder.simpleCheckedTextView.text = devices[position]
 
-        // perform on Click Event Listener on CheckedTextView
         holder.simpleCheckedTextView.setOnClickListener {
+
+            if (!initilized) {
+                lastChanged = holder.simpleCheckedTextView
+                lastChanged.setCheckMarkDrawable(R.drawable.check_ic)
+                initilized = true
+            } else {
+                if (lastChanged != holder.simpleCheckedTextView) {
+                    lastChanged.setCheckMarkDrawable(R.drawable.check_ic)
+                    lastChanged.isChecked = false
+                    lastChanged = holder.simpleCheckedTextView
+                    (context as OrderActivity).order_button.isEnabled = true
+                } else {
+                    (context as OrderActivity).order_button.isEnabled = !(context as OrderActivity).order_button.isEnabled
+                }
+            }
+            OrderActivity.last = holder.simpleCheckedTextView.text.toString()
 
             val value = holder.simpleCheckedTextView.isChecked
             if (value) {
@@ -43,10 +61,7 @@ class AndroidDeviceAdapter(internal var context: Context, private val devices: A
                 Toast.makeText(context, "Checked", Toast.LENGTH_LONG).show()
             }
         }
-
-
     }
-
 
     override fun getItemCount() = devices.size
 }
--- E-Kay-Order/app/src/main/java/a/hw1/ekay/OrderActivity.kt
+++ E-Kay-Order/app/src/main/java/a/hw1/ekay/OrderActivity.kt
@@ -1,22 +1,35 @@
 package a.hw1.ekay
 
-import android.content.DialogInterface
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager;
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_order_activity.*
+import kotlinx.android.synthetic.main.activity_order_activity.view.*
+import android.widget.AdapterView.OnItemClickListener
+
+
 
 class OrderActivity : AppCompatActivity() {
 
-    private val androidDevices = arrayOf("MI5","MI5S","SAMSUNG","NEXUS","ONE PLUS ONE")
+    private val androidDevices = arrayOf("MI5", "MI5S", "SAMSUNG", "NEXUS", "ONE PLUS ONE")
+
+    companion object {
+        var last: String = ""
+        lateinit var orderButton: Button
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_activity)
+
+        orderButton = order_button
 
         // Display welcome message
         val welcomeMsg = resources.getString(R.string.welcome_message, intent.getStringExtra("userName"))
@@ -24,8 +37,13 @@ class OrderActivity : AppCompatActivity() {
 
         // Display devices list
         makeOrder_recycleView.layoutManager = LinearLayoutManager(applicationContext)
-        makeOrder_recycleView.addItemDecoration(DividerItemDecoration(this@OrderActivity,LinearLayoutManager.HORIZONTAL))
-        makeOrder_recycleView.adapter = AndroidDeviceAdapter(applicationContext,androidDevices)
+        makeOrder_recycleView.addItemDecoration(
+            DividerItemDecoration(
+                this@OrderActivity,
+                LinearLayoutManager.HORIZONTAL
+            )
+        )
+        makeOrder_recycleView.adapter = AndroidDeviceAdapter(this@OrderActivity, androidDevices)
 
         // Button Alert-Dialog
         order_button.setOnClickListener {
@@ -33,16 +51,15 @@ class OrderActivity : AppCompatActivity() {
 
             builder
                 .setTitle("E-Kay")
-                .setMessage("Approve order for:" + "?")
-                .setPositiveButton("YES"){_, _ ->
-                    Toast.makeText(applicationContext,"order sent!",Toast.LENGTH_SHORT).show()
+                .setMessage("Approve order for:$last?")
+                .setPositiveButton("YES") { _, _ ->
+                    Toast.makeText(applicationContext, "order sent!", Toast.LENGTH_SHORT).show()
                 }
-                .setNegativeButton("NO"){ _, _ -> }
+                .setNegativeButton("NO") { _, _ -> }
 
             val alertDialog = builder.create()
             alertDialog.show()
         }
-
 
 
     }
