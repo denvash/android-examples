e4a3890cea538b85ec42b17d0411004edeeea865
denvash <justdenva@gmail.com>
2018-11-08 14:35:03 +0200

    Finish OrderActivity and added ExitHelper

--- E-Kay-Order/app/src/main/java/a/hw1/ekay/OrderActivity.kt
+++ E-Kay-Order/app/src/main/java/a/hw1/ekay/OrderActivity.kt
@@ -1,66 +1,63 @@
 package a.hw1.ekay
 
-import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager;
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.DividerItemDecoration
-import android.support.v7.widget.RecyclerView
-import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_order_activity.*
-import kotlinx.android.synthetic.main.activity_order_activity.view.*
-import android.widget.AdapterView.OnItemClickListener
-
+import android.content.Intent
+import android.os.Build
+import android.support.annotation.RequiresApi
 
 
 class OrderActivity : AppCompatActivity() {
 
     private val androidDevices = arrayOf("MI5", "MI5S", "SAMSUNG", "NEXUS", "ONE PLUS ONE")
 
-    companion object {
-        var last: String = ""
-        lateinit var orderButton: Button
+    // Box a string (use it as reference) for order message
+    inner class LastOrder {
+        var str: String = ""
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_activity)
 
-        orderButton = order_button
 
         // Display welcome message
         val welcomeMsg = resources.getString(R.string.welcome_message, intent.getStringExtra("userName"))
         display_user_name.text = welcomeMsg
 
+
         // Display devices list
         makeOrder_recycleView.layoutManager = LinearLayoutManager(applicationContext)
         makeOrder_recycleView.addItemDecoration(
-            DividerItemDecoration(
-                this@OrderActivity,
-                LinearLayoutManager.HORIZONTAL
-            )
+            DividerItemDecoration(this@OrderActivity, LinearLayoutManager.HORIZONTAL)
         )
-        makeOrder_recycleView.adapter = AndroidDeviceAdapter(this@OrderActivity, androidDevices)
+
+        val lastOrder = LastOrder()
+        makeOrder_recycleView.adapter = OrderAdapter(this@OrderActivity, androidDevices, lastOrder)
+
 
         // Button Alert-Dialog
         order_button.setOnClickListener {
-            val builder = AlertDialog.Builder(this@OrderActivity)
-
-            builder
+            AlertDialog.Builder(this@OrderActivity)
                 .setTitle("E-Kay")
-                .setMessage("Approve order for:$last?")
+                .setMessage("Approve order for: ${lastOrder.str}?")
                 .setPositiveButton("YES") { _, _ ->
                     Toast.makeText(applicationContext, "order sent!", Toast.LENGTH_SHORT).show()
                 }
                 .setNegativeButton("NO") { _, _ -> }
-
-            val alertDialog = builder.create()
-            alertDialog.show()
+                .create()
+                .show()
         }
+    }
 
 
+    // Close app at back press
+    override fun onBackPressed() {
+        finishAndRemoveTaskCompat()
     }
 }
